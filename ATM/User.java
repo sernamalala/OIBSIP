5b0279f007cab5a5f591245676ed2fa49108d9b1
sernamalala <sernaprograms@gmail.com>
2024-02-11 16:46:40 +0200

    update User.java with instance variables and methods

--- ATM/User.java
+++ ATM/User.java
@@ -1,3 +1,58 @@
+//User credentials go into this class
+
+import java.security.MessageDigest;
+import  java.util.ArrayList;
+
+
 public class User {
-    
+    //user first name
+    private String firstName;
+    //user last name
+    private String lastName;
+    // user unique id from bank
+    private String userUniqueID;
+    //user pins
+    private byte userPin[];
+    //user accounts ie., cheque, savings
+    private ArrayList<Account> accounts;
+
+    //constructor
+    public User(String firstName, String lastName, String pin, Bank myBank) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        //let's implement a security measure for the user pin
+        //an algorithmic approach
+        //add try and catch
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MDS");
+            this.userPin = md.digest(pin.getBytes());
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            System.exit(1);
+        }
+
+        //generate userUniqueID
+
+        this.userUniqueID = myBank.getUserID();
+
+        //list of accounts
+
+        this.accounts = new ArrayList<Account>();
+
+        //welcome message for account user
+        System.out.println("Welcome " + firstName + " " + lastName);
+        System.out.println("ID: " + this.userUniqueID + "\n");
+    }
+
+    public void addAccount(Account myAccount) {
+        this.accounts.add(myAccount);
+    }
+
+    public  String getUserUniqueID(){
+        return this.userUniqueID;
+    }
 }
+
