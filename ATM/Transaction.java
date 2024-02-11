2d1f646bbbd8f027278f2d92ab3944cbc1ef2f74
sernamalala <sernaprograms@gmail.com>
2024-02-11 16:48:39 +0200

    basic setup of transaction.java nothing too complex done

--- ATM/Transaction.java
+++ ATM/Transaction.java
@@ -1,3 +1,17 @@
-public class Transaction {
-    
+
+import java.util.Date;
+ class Transaction {
+
+    //amount that will be transacted
+
+    private double amount;
+
+    //date to be displayed
+
+    private Date currentTime;
+
+    //current account user is using
+
+     private Account inCurrentAccount;
 }
+
