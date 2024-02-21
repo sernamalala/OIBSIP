b4a23449bbf1fe31d52fa33fdfea94fca48aad51
sernamalala <sernaprograms@gmail.com>
2024-02-21 18:33:31 +0200

    scanner for java project - input from user

---
+++ Number Guessing Game/SernaNumberGuess.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+       
+    }
+}
